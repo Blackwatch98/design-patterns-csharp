1a0b93a659d968cd0b0d615cfa2e9d9de5d39c1c
Daniel Duczymiński <danielduczyminski@gmail.com>
2026-06-02 19:50:27 +0200

    feat(behavioral/strategy): Added naive solution implementation

---
+++ src/Behavioral/Strategy/Strategy.NaiveSolution/OrderPriceCalculator.cs
@@ -0,0 +1,31 @@
+﻿namespace Strategy.NaiveSolution;
+
+public enum CustomerType
+{
+    Regular,
+    Premium,
+    Vip
+}
+
+public class OrderPriceCalculator
+{
+    public decimal CalculateFinalPrice(decimal orderTotal, CustomerType customerType)
+    {
+        if (customerType == CustomerType.Regular)
+        {
+            return orderTotal;
+        }
+
+        if (customerType == CustomerType.Premium)
+        {
+            return orderTotal * 0.90m;
+        }
+
+        if (customerType == CustomerType.Vip)
+        {
+            return orderTotal * 0.80m;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(customerType));
+    }
+}
