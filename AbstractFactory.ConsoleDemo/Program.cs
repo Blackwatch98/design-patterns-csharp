3929d7fbdac792b2ca0417b49d3a7cb71a0d2df4
Daniel Duczymiński <danielduczyminski@gmail.com>
2026-06-02 21:05:10 +0200

    test(creational/abstract-factory): Added naive abstract factory renderer tests and console example execute

--- AbstractFactory.ConsoleDemo/Program.cs
+++ AbstractFactory.ConsoleDemo/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using NaiveUiRenderer = AbstractFactory.NaiveSolution.UiRenderer;
+using NaiveUiTheme = AbstractFactory.NaiveSolution.UiTheme;
+
+var renderer = new NaiveUiRenderer();
+
+Console.WriteLine("Abstract Factory - Naive Solution");
+Console.WriteLine("---------------------------------");
+
+RenderUi(NaiveUiTheme.Windows);
+Console.WriteLine();
+RenderUi(NaiveUiTheme.MacOs);
+
+void RenderUi(NaiveUiTheme theme)
+{
+    Console.WriteLine($"Theme: {theme}");
+    Console.WriteLine(renderer.Render(theme));
+}
---
+++ AbstractFactory.Tests/NaiveSolution/UiRendererTests.cs
@@ -0,0 +1,34 @@
+﻿using Shouldly;
+using UiRenderer = AbstractFactory.NaiveSolution.UiRenderer;
+using UiTheme = AbstractFactory.NaiveSolution.UiTheme;
+
+namespace AbstractFactory.Tests.NaiveSolution;
+
+public class UiRendererTests
+{
+    [Fact]
+    public void Render_ForWindowsTheme_ReturnsWindowsComponents()
+    {
+        var renderer = new UiRenderer();
+
+        var result = renderer.Render(UiTheme.Windows);
+
+        result.ShouldBe($"""
+                     Rendering Windows button
+                     Rendering Windows checkbox
+                     """);
+    }
+
+    [Fact]
+    public void Render_ForMacOsTheme_ReturnsMacOsComponents()
+    {
+        var renderer = new UiRenderer();
+
+        var result = renderer.Render(UiTheme.MacOs);
+
+        result.ShouldBe($"""
+                     Rendering MacOS button
+                     Rendering MacOS checkbox
+                     """);
+    }
+}
