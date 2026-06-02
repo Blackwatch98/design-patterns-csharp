53f199b15b2d128fe1af811297a2a533e14f49a9
Daniel Duczymiński <danielduczyminski@gmail.com>
2026-06-02 20:30:35 +0200

    test(behavioral/strategy): Added strategy pattern solution tests

---
+++ src/Behavioral/Strategy/Strategy.Tests/NaiveSolution/OrderPriceCalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using Shouldly;
+using Strategy.NaiveSolution;
+
+using OrderPriceCalculator = Strategy.NaiveSolution.OrderPriceCalculator;
+
+namespace Strategy.Tests.NaiveSolution;
+
+public class OrderPriceCalculatorTests
+{
+    [Fact]
+    public void CalculateFinalPrice_ForRegularCustomer_ReturnsFullPrice()
+    {
+        var calculator = new OrderPriceCalculator();
+
+        var result = calculator.CalculateFinalPrice(100m, CustomerType.Regular);
+
+        result.ShouldBe(100m);
+    }
+
+    [Fact]
+    public void CalculateFinalPrice_ForPremiumCustomer_ReturnsPriceWithTenPercentDiscount()
+    {
+        var calculator = new OrderPriceCalculator();
+
+        var result = calculator.CalculateFinalPrice(100m, CustomerType.Premium);
+
+        result.ShouldBe(90m);
+    }
+
+    [Fact]
+    public void CalculateFinalPrice_ForVipCustomer_ReturnsPriceWithTwentyPercentDiscount()
+    {
+        var calculator = new OrderPriceCalculator();
+
+        var result = calculator.CalculateFinalPrice(100m, CustomerType.Vip);
+
+        result.ShouldBe(80m);
+    }
+}
---
+++ src/Behavioral/Strategy/Strategy.Tests/PatternSolution/OrderPriceCalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using Shouldly;
+using Strategy.PatternSolution.Discounts;
+
+using OrderPriceCalculator = Strategy.PatternSolution.OrderPriceCalculator;
+
+namespace Strategy.Tests.PatternSolution;
+
+public class OrderPriceCalculatorTests
+{
+    [Fact]
+    public void CalculateFinalPrice_WithRegularDiscountStrategy_ReturnsFullPrice()
+    {
+        var calculator = new OrderPriceCalculator(new RegularDiscountStrategy());
+
+        var result = calculator.CalculateFinalPrice(100m);
+
+        result.ShouldBe(100m);
+    }
+
+    [Fact]
+    public void CalculateFinalPrice_WithPremiumDiscountStrategy_ReturnsPriceWithTenPercentDiscount()
+    {
+        var calculator = new OrderPriceCalculator(new PremiumDiscountStrategy());
+
+        var result = calculator.CalculateFinalPrice(100m);
+
+        result.ShouldBe(90m);
+    }
+
+    [Fact]
+    public void CalculateFinalPrice_WithVipDiscountStrategy_ReturnsPriceWithTwentyPercentDiscount()
+    {
+        var calculator = new OrderPriceCalculator(new VipDiscountStrategy());
+
+        var result = calculator.CalculateFinalPrice(100m);
+
+        result.ShouldBe(80m);
+    }
+}
