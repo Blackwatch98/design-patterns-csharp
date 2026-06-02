1a53f932f5cef367dd66792d8278be57d64a14cf
Daniel Duczymiński <danielduczyminski@gmail.com>
2026-06-02 19:59:17 +0200

    test(behavioral/strategy): Added naive strategy example tests

---
+++ src/Behavioral/Strategy/Strategy.Tests/StrategyUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using Shouldly;
+using Strategy.NaiveSolution;
+
+namespace Strategy.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CalculateFinalPrice_ForRegularCustomer_ReturnsFullPrice()
+        {
+            var calculator = new OrderPriceCalculator();
+
+            var result = calculator.CalculateFinalPrice(100m, CustomerType.Regular);
+
+            result.ShouldBe(100m);
+        }
+
+        [Fact]
+        public void CalculateFinalPrice_ForPremiumCustomer_ReturnsPriceWithTenPercentDiscount()
+        {
+            var calculator = new OrderPriceCalculator();
+
+            var result = calculator.CalculateFinalPrice(100m, CustomerType.Premium);
+
+            result.ShouldBe(90m);
+        }
+
+        [Fact]
+        public void CalculateFinalPrice_ForVipCustomer_ReturnsPriceWithTwentyPercentDiscount()
+        {
+            var calculator = new OrderPriceCalculator();
+
+            var result = calculator.CalculateFinalPrice(100m, CustomerType.Vip);
+
+            result.ShouldBe(80m);
+        }
+    }
+}
